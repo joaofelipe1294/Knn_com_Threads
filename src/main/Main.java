27da00a4f693744dd34d94b387317f76ce4b27ae
João Felipe Lopes de Sus <joaofelipe1294@gmail.com>
2016-03-24 21:48:00 -0300

    Concluida a quebra do lista com os valores de teste em listas menores

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ package main;
 import beans.Ponto;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.ListIterator;
 import threads.LeArquivoThread;
@@ -19,21 +20,40 @@ import threads.ProcessaPontosThread;
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        final int NUMERO_THREADS = 2;
+        final int NUMERO_THREADS = 3;
         int kMaisProximos = 5;
-        LeArquivoThread runnableTrain = new LeArquivoThread("150k/treino");
+        LeArquivoThread runnableTrain = new LeArquivoThread("150k/CCtrain");
         Thread threadTrain = new Thread(runnableTrain);
-        LeArquivoThread runnableTest = new LeArquivoThread("150k/teste");
+        LeArquivoThread runnableTest = new LeArquivoThread("150k/CCtest1");
         Thread threadTest = new Thread(runnableTest);
         threadTrain.start();
         threadTest.start();
         threadTrain.join();
         threadTest.join();        
-        
         List<String> listaTrain = runnableTrain.getLista();
         List<String> listaTest = runnableTest.getLista();
         List<List<String>> listas = new ArrayList<>();
-        for(int contadorListas = 0 ; contadorListas < listaTest.size() / NUMERO_THREADS ; contadorListas++){
+        int contadorTeste = 0;
+        for(int contadorListas = 0 ; contadorListas < NUMERO_THREADS ; contadorListas++){
+            listas.add(new ArrayList<>());
+            int indexLimite;
+            if((contadorListas == (NUMERO_THREADS - 1) && (listaTest.size() % NUMERO_THREADS) > 0)){
+                indexLimite = listaTest.size() - 1;
+            }else{
+                indexLimite = ((listaTest.size() / NUMERO_THREADS) * (contadorListas + 1)) - 1;
+            }
+            while(contadorTeste <= indexLimite){
+                listas.get(contadorListas).add(listaTest.get(contadorTeste));
+                contadorTeste++;
+            }
+        }
+        
+        for(List<String> lista : listas){
+            System.out.println(lista.size());
+        }
+        
+        
+        /*for(int contadorListas = 0 ; contadorListas < listaTest.size() / NUMERO_THREADS ; contadorListas++){
             listas.add(new ArrayList<>());
             int limiteLoop = (NUMERO_THREADS * ( contadorListas)) - 1;
             for(int contadorListaTest = 0 ; contadorListaTest < limiteLoop ; contadorListaTest++){
@@ -42,9 +62,9 @@ public class Main {
         }
         for(String s : listas.get(1)){
             System.out.println(s);
-        }
+        }*/
         
-        List<String> test1 = new ArrayList<>();
+        //List<String> test1 = new ArrayList<>();
         //ProcessaPontosThread run = new ProcessaPontosThread(listaTrain, test1 , kMaisProximos);
         //Thread t1 = new Thread(run);
         //t1.start();
